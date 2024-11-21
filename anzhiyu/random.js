2e47a44ffcb48ea89e8f50c4b0d1ec59edcbfa54
ehchhhhe <fliegen-bird@outlook.com>
2024-11-22 06:00:38 +0800

    Site updated: 2024-11-22 06:00:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/9b8313e8.html","posts/4a17b156.html","posts/2e5b4759.html","posts/74db913a.html"];function toRandomPost(){
+var posts=["posts/9b8313e8.html","posts/4a17b156.html","posts/74db913a.html","posts/2e5b4759.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
